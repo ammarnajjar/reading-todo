67322edc10fd85c7b0dd25c1f59e49491c90d381
Ammar Najjar <najjarammar@protonmail.com>
2019-08-23 02:52:12 +0200

    feat(game): first state from the board game
    https://codepen.io/gaearon/pen/oWWQNa?editors=0010
    
    Issue #14

---
+++ client/src/Square.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Square.css';
+
+export class Square extends React.Component {
+  render() {
+    return <button className="square">{/* TODO */}</button>;
+  }
+}
