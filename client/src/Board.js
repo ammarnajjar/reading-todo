96e05d6938a3c63522069a0afe808a92407b0745
Ammar Najjar <najjarammar@protonmail.com>
2019-08-23 12:33:06 +0200

    test(Game): add should render tests for all compoonents
    Issue #14

--- client/src/Board.js
+++ client/src/Board.js
@@ -3,7 +3,7 @@ import { Square } from './Square.js';
 import './Board.css';
 
 export class Board extends React.Component {
-  renderSquare(i) {
+  renderSquare() {
     return <Square />;
   }
 
@@ -14,19 +14,19 @@ export class Board extends React.Component {
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this.renderSquare()}
+          {this.renderSquare()}
+          {this.renderSquare()}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {this.renderSquare()}
+          {this.renderSquare()}
+          {this.renderSquare()}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {this.renderSquare()}
+          {this.renderSquare()}
+          {this.renderSquare()}
         </div>
       </div>
     );
