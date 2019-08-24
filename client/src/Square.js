cc223b18537fb3f84073b8226edbae555989e586
Ammar Najjar <najjarammar@protonmail.com>
2019-08-24 13:58:31 +0200

    feat(react): replace stateless Square class with a function
    Issue #14

--- client/src/Square.js
+++ client/src/Square.js
@@ -2,20 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Square.css';
 
-export class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+export function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 Square.propTypes = {
