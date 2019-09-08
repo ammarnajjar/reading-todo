38afbe1458cd2b70ffd16f0b3f2e0a684e03b567
Ammar Najjar <najjarammar@protonmail.com>
2019-09-08 23:47:35 +0200

    style(initdb): format sql
    Issue #04

--- db/docker-entrypoint-initdb.d/init.sql
+++ db/docker-entrypoint-initdb.d/init.sql
@@ -1,3 +1,9 @@
 CREATE TABLE test (id serial PRIMARY KEY, title varchar);
-INSERT INTO test (title) VALUES ('Test Title Book');
-SELECT * FROM test;
+INSERT INTO
+  test (title)
+VALUES
+  ('Test Title Book');
+SELECT
+  *
+FROM
+  test;
