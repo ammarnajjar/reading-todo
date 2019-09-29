b1b7ff6dfaa46f142e30766b0eaac46a20840823
Ammar Najjar <najjarammar@protonmail.com>
2019-09-29 17:28:50 +0200

    feat(database): add reading_todo schema
    Issue #4

--- db/docker-entrypoint-initdb.d/init.sql
+++ db/docker-entrypoint-initdb.d/init.sql
@@ -1,25 +1,28 @@
-DROP TABLE IF EXISTS books;
-CREATE TABLE books (
+CREATE SCHEMA readiing_todo;
+DROP TABLE IF EXISTS readiing_todo.books;
+CREATE TABLE readiing_todo.books (
   isbn varchar(50) PRIMARY KEY,
   title varchar(300),
   year char(4),
   category varchar(300),
   sub_category varchar(300)
 );
-DROP TABLE IF EXISTS authors;
-CREATE TABLE authors (isbn varchar(50), name varchar(300));
+COMMENT ON TABLE readiing_todo.books IS 'Book to read';
+DROP TABLE IF EXISTS readiing_todo.authors;
+CREATE TABLE readiing_todo.authors (isbn varchar(50), name varchar(300));
+COMMENT ON TABLE readiing_todo.authors IS 'Auther of the book';
 ALTER TABLE
-  authors
+  readiing_todo.authors
 ADD
-  FOREIGN KEY (isbn) REFERENCES books (isbn);
+  FOREIGN KEY (isbn) REFERENCES readiing_todo.books (isbn);
 SELECT
   *
 FROM
-  books;
+  readiing_todo.books;
 SELECT
   *
 FROM
-  authors;
+  readiing_todo.authors;
 -- Show databases
 SELECT
   *
