da57f68b7bbbadc1a93a29fec34af79f2a6d6c88
Ammar Najjar <najjarammar@protonmail.com>
2019-09-30 19:11:59 +0200

    fix(database): use three tables instead of two
    Issue #04

--- db/docker-entrypoint-initdb.d/init.sql
+++ db/docker-entrypoint-initdb.d/init.sql
@@ -1,28 +1,39 @@
 CREATE SCHEMA readiing_todo;
-DROP TABLE IF EXISTS readiing_todo.books;
-CREATE TABLE readiing_todo.books (
+DROP TABLE IF EXISTS readiing_todo.book;
+CREATE TABLE readiing_todo.book (
   isbn varchar(50) PRIMARY KEY,
   title varchar(300),
   year char(4),
   category varchar(300),
   sub_category varchar(300)
 );
-COMMENT ON TABLE readiing_todo.books IS 'Book to read';
-DROP TABLE IF EXISTS readiing_todo.authors;
-CREATE TABLE readiing_todo.authors (isbn varchar(50), name varchar(300));
-COMMENT ON TABLE readiing_todo.authors IS 'Auther of the book';
+COMMENT ON TABLE readiing_todo.book IS 'Book to read';
+DROP TABLE IF EXISTS readiing_todo.author;
+CREATE TABLE readiing_todo.author (id bigserial PRIMARY KEY, name varchar(300));
+COMMENT ON TABLE readiing_todo.author IS 'Auther of the book';
+DROP TABLE IF EXISTS readiing_todo.book_author;
+CREATE TABLE readiing_todo.book_author (book_isbn varchar(50), author_id bigint);
+COMMENT ON TABLE readiing_todo.book_author IS 'Book <-> Author';
 ALTER TABLE
-  readiing_todo.authors
+  readiing_todo.book_author
 ADD
-  FOREIGN KEY (isbn) REFERENCES readiing_todo.books (isbn);
+  FOREIGN KEY (book_isbn) REFERENCES readiing_todo.book (isbn);
+ALTER TABLE
+  readiing_todo.book_author
+ADD
+  FOREIGN KEY (author_id) REFERENCES readiing_todo.author (id);
 SELECT
   *
 FROM
-  readiing_todo.books;
+  readiing_todo.book;
 SELECT
   *
 FROM
-  readiing_todo.authors;
+  readiing_todo.author;
+SELECT
+  *
+FROM
+  readiing_todo.book_author;
 -- Show databases
 SELECT
   *
