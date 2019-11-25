fd4c8bd524020496ea4eb75b045951a15b276d56
Ammar Najjar <najjarammar@protonmail.com>
2019-11-25 10:48:05 +0100

    build: move to yarn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,55 @@
+import PropTypes from 'prop-types';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Square extends React.Component<{ value: string }> {
+  static propTypes = {
+    value: PropTypes.string,
+  };
+  render() {
+    return <button className="square">{this.props.value}</button>;
+  }
+}
+
+interface BoardState {
+  squares: string[];
+}
+
+class Board extends React.Component<any, BoardState> {
+  static propTypes = {
+    squares: PropTypes.array,
+  };
+  constructor(props: BoardState) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill('null'),
+    };
+  }
+  render() {
+    const status = 'Next Player: X';
+    return (
+      <div>
+        <div className="status">{status}</div>
+        {[0, 3, 6].map(i => (
+          <div key={i} className="board-row">
+            {[i, i + 1, i + 2].map(j => this.renderSquare(j))}
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  handleClick(i: number): void {
+    return;
+  }
+
+  renderSquare(i: number): any {
+    const value = this.state.squares[i];
+    return <Square key={i} value={value} />;
+  }
+}
+
+const App: React.FunctionComponent = () => {
+  return <Board />;
 };
 
 export default App;
