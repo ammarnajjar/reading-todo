454e984fbd33ad14a762cb3917d839c4da32008d
Ammar Najjar <najjarammar@protonmail.com>
2020-07-27 14:32:39 +0200

    refactor: remove dead code (mock books list)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,23 +2,7 @@ import React from 'react';
 import { BooksTable } from './books-table';
 import { BookInDB } from './models';
 
-const books: BookInDB[] = [
-  // {
-  //   id: 1,
-  //   author: 'auth1',
-  //   title: 'title1',
-  // },
-  // {
-  //   id: 2,
-  //   author: 'auth2',
-  //   title: 'This is a very long book title for testing',
-  // },
-  // {
-  //   id: 999999,
-  //   author: 'This is a very long book author name for testing',
-  //   title: 'title3',
-  // },
-];
+const books: BookInDB[] = [];
 
 export const App: React.FunctionComponent = () => {
   return <BooksTable books={books} />;
