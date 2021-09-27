2d0097ac270443142f910d8f690f7eed24ae1bab
Ammar Najjar <najjarammar@protonmail.com>
2021-09-27 21:59:51 +0200

    feat: graphql playground working

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [BooksModule],
+  imports: [
+    BooksModule,
+    GraphQLModule.forRoot({
+      debug: false,
+      playground: true,
+      autoSchemaFile: 'schema.gql',
+      installSubscriptionHandlers: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- api/src/books/entities/book.entity.ts
+++ api/src/books/entities/book.entity.ts
@@ -1,7 +1,22 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Field, Int, ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
 export class Book {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => Int, { description: 'book id' })
+  id: number;
+
+  @Field(() => String, { description: 'book title' })
+  title: string;
+
+  @Field(() => String, { nullable: true, description: 'book isbn' })
+  isbn: string;
+
+  @Field(() => String, { nullable: true, description: 'book author' })
+  author: string;
+
+  @Field(() => String, { nullable: true, description: 'book category' })
+  category: string;
+
+  @Field(() => Int, { nullable: true, description: 'book year' })
+  year: number;
 }
